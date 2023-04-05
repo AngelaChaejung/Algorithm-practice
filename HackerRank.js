2a5adeb1b81cb23e661b7828a2f5e586f3f76773
Angelachaejung <a01083298599@gmail.com>
2023-04-05 10:58:29 +0900

    4/APR

---
+++ HackerRank.js
@@ -0,0 +1,6 @@
+function solveMeFirst(a, b) {
+  answer = a + b;
+  console.log(answer);
+  return answer;
+}
+solveMeFirst(2, 3);
---
+++ test.ts
@@ -0,0 +1,6 @@
+function solveMeFirst1(a: number, b: number): number {
+  const answer = a + b;
+  console.log(answer);
+  return answer;
+}
+solveMeFirst1(3, 2);
