0162d276e0b7babe22a107a701329d7ecd84f5a8
Angelachaejung <a01083298599@gmail.com>
2023-04-15 15:14:28 +0900

    Update MigratoryBirds.js

--- hackerRank/MigratoryBirds.js
+++ hackerRank/MigratoryBirds.js
@@ -2,20 +2,37 @@
 // 만약 하나의 타입 이상이 최대치에 spot된다면 그들의 가장 작은 id를 return.
 
 function migratoryBirds(arr) {
+  let maxdup = 0;
+  let maxdupelement = 0;
+  let samemaxdup = [];
   const duplicates = arr.reduce((acc, item) => {
     if (arr.indexOf(item) !== arr.lastIndexOf(item) && !acc.includes(item)) {
       acc.push(item);
     }
     return acc;
   }, []);
-  console.log(duplicates);
-  for (let i = 0; i < duplicates.length; i++) {
-    const count = arr.filter((item) => item === duplicates[i]).length;
-    console.log(i, ":", count);
-  }
-}
 
-console.log(migratoryBirds([1, 4, 4, 4, 5, 3, 1, 1, 1]));
+  for (let i = 0; i < duplicates.length; i++) {
+    const dupcount = arr.filter((item) => item === duplicates[i]).length;
+    if (dupcount > maxdup) {
+      maxdup = dupcount;
+      maxdupelement = duplicates[i];
+    }
+
+    console.log(duplicates[i], ":", dupcount);
+    console.log(maxdupelement, maxdup);
+    console.log(samemaxdup);
+  }
+  for (let i = 0; i < duplicates.length; i++) {
+    const dupcount = arr.filter((item) => item === duplicates[i]).length;
+    if (dupcount === maxdup) {
+      samemaxdup.push(duplicates[i]);
+    }
+  }
+  console.log(samemaxdup);
+  return Math.min(maxdupelement);
+}
+console.log(migratoryBirds([1, 4, 4, 4, 4, 5, 3, 1, 1, 1]));
 
 // example. migratoryBirds([1, 1, 2, 2, 3])
 // 타입1,2 그리고 하나의 sighting의 3이있다.
